e6d0f8ff5333c5ab03754b63fc768879be80ac8a
Richard Abrams <rdabrams501@gmail.com>
2026-08-18 16:58:22 -0400

    Fix collapsible navbar

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router'
 import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
